4e55262c3c70976850442cc664e3ec6b0c1b28fe
raimeiga <yusuke612@hotmail.com>
2023-10-13 00:14:11 +0900

    11.6 ディレクティブを使用

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,8 +10,14 @@
  <body>
      <p>Hello, world!</p>
      <p>こんにちは、{{ $name }}さん！</p>  <!-- $nameはコントローラから受け取った変数 -->
-      
+      <ul>
+          @foreach($languages as $language)
+              <li>{{ $language }}</li>
+          @endforeach
+      </ul>
  </body>
-           
+           <!-- ↑ Bladeテンプレート内ではifやforeachの前にアットマークをつけるだけで、
+                簡単にPHPやLaravelの処理を実行できる。アットマークから始まるコード
+                のことを、Laravelではディレクティブという（directive=命令って意味）。 -->
  
  </html>
