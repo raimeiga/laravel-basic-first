ea0008d2262df366b2bfd2586bcff6480225a60c
raimeiga <yusuke612@hotmail.com>
2023-10-14 11:31:45 +0900

    15.4 クエリビルダを使ってみよう/ビュー作成

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Laravel基礎</title>
+ </head>
+ 
+ <body>
+     <table>
+         <tr>
+             <th>ID</th>
+             <th>商品名</th>
+             <th>価格</th>
+             <th>作成日時</th>
+             <th>更新日時</th>
+         </tr>
+         @foreach($products as $product)  
+             <tr>
+                 <td>{{ $product->id }}</td>
+                 <td>{{ $product->product_name }}</td>
+                 <td>{{ $product->price }}</td>
+                 <td>{{ $product->created_at }}</td>
+                 <td>{{ $product->updated_at }}</td>
+             </tr>            
+         @endforeach 
+                <!-- ↑ $productsはProductController.php（コントローラのファイル）上に
+                　　 　書いた変数で、内容はDB::table('products')->get();       -->
+     </table>
+ </body>
+ 
+ </html>
