df9855673b9b124e86c462d2b538d00454a3d8f7
raimeiga <yusuke612@hotmail.com>
2023-10-15 17:33:53 +0900

    18.4リレーションシップを設定/Vendorコントローラの作成

---
+++ app/Http/Controllers/VendorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VendorController extends Controller
+{
+    public function show($id) {
+        // URL'/vendors/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをvendorsテーブルから取得し、変数$vendorに代入する
+        $vendor = Vendor::find($id);
+
+        // インスタンスに紐づくproductsテーブルのすべてのデータをインスタンスのコレクションとして取得する
+        $products = $vendor->products;
+
+        // 変数$vendorと変数$productsをvendors/show.blade.phpファイルに渡す
+        return view('vendors.show', compact('vendor', 'products'));
+    }    
+}
