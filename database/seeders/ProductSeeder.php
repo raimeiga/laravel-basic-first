7f75cd8c21a7b37f5f878c768f31fb61ea2b48bc
raimeiga <yusuke612@hotmail.com>
2023-10-17 08:54:07 +0900

    19.4 シーディングを利用してダミーデータを追加/シーディングの実行

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 
 class ProductSeeder extends Seeder
 {
@@ -13,6 +13,12 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('products')->insert([
+            'product_name' => 'ノート5冊セット',
+            'price' => 600,
+            'created_at' => '2023-06-01 00:00:00',
+            'updated_at' => '2023-06-01 00:00:00',
+            'vendor_code' => 1111
+        ]);
     }
 }
