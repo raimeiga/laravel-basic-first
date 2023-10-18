db4c28ff2eae7e19aafaa3f900e64190d458d96a
raimeiga <yusuke612@hotmail.com>
2023-10-18 18:04:26 +0900

    リクエスト情報の取得方法の説明追記

--- app/Http/Controllers/RequestController.php
+++ app/Http/Controllers/RequestController.php
@@ -10,7 +10,13 @@ class RequestController extends Controller
     public function create() {
         return view('requests.create');
     } 
-
+             /* リクエスト情報を取得する方法
+             　 コントローラのアクションの引数に$request（前のRequestは型宣言）を記述すると、
+                Laravelは自動的に、そのリクエスト情報を含むRequestクラスのインスタンスを生成する。
+               （$requestは変数だが、その中身（インスタンス）は、ユーザー（ブラウザか）ら要求され
+                る情報を含めた大量の情報らしい。）そしてアクション内で$requestの中身を取得したり
+                して使うことができる。↓↓↓↓↓↓↓↓↓↓↓↓↓
+             */
     public function confirm(Request $request) {
         // HTTPリクエストに含まれる、単一のパラメータの値を取得する     
         $user_name = $request->input('user_name');
