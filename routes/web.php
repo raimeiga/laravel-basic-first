404f1ac569f1fb09679c077b12f9994b271761f9
raimeiga <yusuke612@hotmail.com>
2023-10-22 07:49:06 +0900

    Laravel Breezeパッケージのインストール

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,8 @@
- <?php
+<?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-/* ルーティングを設定するコントローラを宣言「HelloControllerというコントローラを使うよ！」
-   HelloController.phpファイルにアクションを記述するよう想起する
-*/
 
-use App\Http\Controllers\HelloController;      //「HelloControllerを使うよ」宣言
-use App\Http\Controllers\ProductController;    //「ProductControllerを使うよ」宣言
-use App\Http\Controllers\VendorController;
-use App\Http\Controllers\RequestController;
-use App\Http\Controllers\ResponseController;
-use App\Http\Controllers\CookieController;
-use App\Http\Controllers\SessionController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,41 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', [HelloController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/products', [ProductController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/products/create', [ProductController::class, 'create']);
- 
-Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
-
-Route::get('/products/{id}', [ProductController::class, 'show']);
-
-Route::get('/vendors/create', [VendorController::class, 'create']);
- 
-Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
-
-Route::get('/vendors/{id}', [VendorController::class, 'show']);
-
-Route::get('/requests/create', [RequestController::class, 'create']);
- 
-Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
-
-Route::get('/responses', [ResponseController::class, 'index']);
-
-Route::get('/cookies', [CookieController::class, 'index']);
- 
-Route::get('/cookies/create', [CookieController::class, 'create'])->name('cookies.create');
- 
-Route::post('/cookies/store', [CookieController::class, 'store'])->name('cookies.store');
- 
-Route::delete('/cookies/destroy', [CookieController::class, 'destroy'])->name('cookies.destroy');
-
-Route::get('/sessions', [SessionController::class, 'index']);
- 
-Route::get('/sessions/create', [SessionController::class, 'create'])->name('sessions.create');
- 
-Route::post('/sessions/store', [SessionController::class, 'store'])->name('sessions.store');
- 
-Route::delete('/sessions/destroy', [SessionController::class, 'destroy'])->name('sessions.destroy');
-
+require __DIR__.'/auth.php';
